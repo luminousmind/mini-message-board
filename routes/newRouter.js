ae9a379c650e8819d48f979f2b2ab6b3694efb09
luminousmind <193550240+luminousmind@users.noreply.github.com>
2025-01-06 16:23:32 +1100

    feat: add system to update messages after form submit

--- routes/newRouter.js
+++ routes/newRouter.js
@@ -1,13 +1,19 @@
 const { Router } = require("express");
 const newRouter = Router();
+const messages = require("./indexRouter");
 
 newRouter.get("/", (req, res) => {
     res.render("form");
 });
 
 newRouter.post("/", (req, res) => {
-    res.send(`The author's name is: ${req.body.author} and 
-        your message is: ${req.body.message}`)
+    messages.push({
+        text: req.body.message,
+        user: req.body.author,
+        added: new Date()
+    });
+
+    res.redirect("/");
 });
 
 module.exports = newRouter;
