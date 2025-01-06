3ae17215d6eb75f26406c0849217753cb99c9a41
luminousmind <193550240+luminousmind@users.noreply.github.com>
2025-01-06 15:47:27 +1100

    feat: form functionality and retriving data

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 8080;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/", indexRouter);
 app.use("/new", newRouter);
 
--- routes/newRouter.js
+++ routes/newRouter.js
@@ -2,7 +2,12 @@ const { Router } = require("express");
 const newRouter = Router();
 
 newRouter.get("/", (req, res) => {
-    res.send("New message!");
+    res.render("form");
+});
+
+newRouter.post("/", (req, res) => {
+    res.send(`The author's name is: ${req.body.author} and 
+        your message is: ${req.body.message}`)
 });
 
 module.exports = newRouter;
